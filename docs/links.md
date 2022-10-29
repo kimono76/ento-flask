87b921ee196dbc770613cbeb1aa4b89d29c081b9
vi-think-11 <alberto.gruning.zen@gmail.com>
2022-10-29 12:42:18 -0300

    feat:add a route for post with rest client

---
+++ docs/links.md
@@ -0,0 +1,4 @@
+[anaconda installer][anaconda]
+
+
+[anaconda]: https://www.anaconda.com/products/distribution
--- server.py
+++ server.py
@@ -2,6 +2,16 @@ from flask import Flask
 
 app= Flask(__name__)
 
+
+try:
+    pass
+except:
+    pass
+
+@app.route('/users',methods=['POST'])
+def create_user():
+    return'POST create user'
+
 if __name__== '__main__':
     app.run(port=3000,debug=True)
 
